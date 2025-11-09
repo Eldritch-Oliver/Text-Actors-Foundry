bd301d69fb1e99017fd4151468bf32101847820d
Oliver <oliver@noreply.localhost>
2025-11-08 19:06:19 -0700

    Implement the chat notification for the player

--- module/sockets/createNotif.mjs
+++ module/sockets/createNotif.mjs
@@ -1,1 +1,19 @@
-export function createNotif() {};
+export function createNotif(payload) {
+	const { userID, content, includeGM } = payload;
+
+	if (userID !== game.user.id) { return };
+
+	// TODO: prevent this from working if the user hasn't submitted a query response
+
+	let whisper = [game.user.id];
+	if (includeGM) {
+		whisper = game.users.filter(u => u.isGM).map(u => u.id);
+	};
+
+	ChatMessage.implementation.create({
+		flavor: `Data Query Notification`,
+		content,
+		whisper,
+		style: CONST.CHAT_MESSAGE_STYLES.OOC,
+	});
+};

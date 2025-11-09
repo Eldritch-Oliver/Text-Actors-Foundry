db4f57fc905662592ef138f23a7406e19fbc0dd4
Oliver <oliver@noreply.localhost>
2025-11-08 19:11:52 -0700

    Add the special case for prompting all but the requesting user

--- module/sockets/dataRequest.mjs
+++ module/sockets/dataRequest.mjs
@@ -20,26 +20,32 @@ export async function dataRequest(payload) {
 	};
 
 	// null/undefined is a special case for "all users but me" by default
-	if (!Array.isArray(users) || users != null) {
+	if (users != null && !Array.isArray(users)) {
 		ui.notifications.error(game.i18n.format(
 			`taf.notifs.error.malformed-socket-payload`,
 			{
 				event: `query.prompt`,
-				details: ``,
+				details: `A list of users must be provided`,
 			}),
 		);
 		return;
 	};
 
-	if (!users.includes(game.user.id)) { return };
+	if (users != null && !users.includes(game.user.id)) { return };
 
 	request.id = id;
 	const result = await DialogManager.ask(request, config);
-	if (result.state === `fronted`) { return };
-	if (result.state === `errored`) {
-		ui.notifications.error(result.error);
+	if (result.state === `fronted`) {
 		return;
+	} else if (result.state === `errored`) {
+		ui.notifications.error(result.error);
+	} else if (result.state === `prompted`) {
+		game.socket.emit(`system.taf`, {
+			event: `query.submit`,
+			payload: {
+				id: request.id,
+				answers: result.answers,
+			},
+		});
 	};
-
-
 };
